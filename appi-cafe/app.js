574dfe7004118a2db3259e0070704cc566265ad0
SteelFenix09 <f_771@outlook.com>
2025-03-26 01:11:26 -0600

    cs

---
+++ appi-cafe/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const productoRoute = require("./routes/Producto.routes")
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+//app.use(express.static('uploads'))
+app.use(express.static(path.join(__dirname, 'uploads')))
+
+app.use(cors())
+
+app.use('/api', productoRoute)
+
+module.exports = app
