76949db7b3dd6eda08ca6d7bd7f880eced83fc47
SteelFenix09 <f_771@outlook.com>
2025-03-26 22:31:38 -0600

    Renderizado

--- appi/src/api/producto.js
+++ appi/src/api/producto.js
@@ -19,7 +19,7 @@ export class Producto {
                     headers: { "Content-Type": "multipart/form-data" },
                 }
             );
-            console.log("Producto creado correctamente:", response.data);
+            //console.log("Producto creado correctamente:", response.data);
             return response.data; // Devuelve los datos del producto creado
         } catch (error) {
             console.error("Error al crear el producto:", error.message);
@@ -32,7 +32,7 @@ export class Producto {
         try {
             const url = `${this.baseApi}${ENV.API_ROUTES.GETPRODUCTOS}`;
             const response = await Axios.get(url);
-            console.log("Productos obtenidos correctamente:", response.data);
+            //console.log("Productos obtenidos correctamente:", response.data);
             return response.data; // Devuelve la lista de productos
         } catch (error) {
             console.error("Error al obtener los productos:", error.message);
@@ -45,7 +45,7 @@ export class Producto {
         try {
             const url = `${this.baseApi}${ENV.API_ROUTES.DELETEPRODUCTO}/${id}`;
             await Axios.delete(url);
-            console.log(`Producto con ID ${id} eliminado correctamente.`);
+            //console.log(`Producto con ID ${id} eliminado correctamente.`);
         } catch (error) {
             console.error(`Error al eliminar el producto con ID ${id}:`, error.message);
             throw new Error("No se pudo eliminar el producto. Intenta nuevamente.");
@@ -67,7 +67,7 @@ export class Producto {
                     headers: { "Content-Type": "multipart/form-data" },
                 }
             );
-            console.log(`Producto con ID ${id} actualizado correctamente:`, response.data);
+            //console.log(`Producto con ID ${id} actualizado correctamente:`, response.data);
             return response.data; // Devuelve los datos del producto actualizado
         } catch (error) {
             console.error(`Error al actualizar el producto con ID ${id}:`, error.message);
